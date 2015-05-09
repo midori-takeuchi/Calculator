6a7cdcc3ed4c1afc8c23f514d2048ca4d2c25652
Midori Takeuchi <midoribeth@hotmail.com>
2015-05-09 15:11:01 -0700

    Update HangMan.cc
    Program now updated and working properly. 
    Will try to output an actual hangman image with each guess.

--- HangMan.cc
+++ HangMan.cc
@@ -10,35 +10,37 @@ int hangMan(string word) {
 	for (int i = 0; i < output.length(); i++) {
 		output[i] = '_';
 	}
+	// setting # of tries
 	while (numberGuessed < word.length()) {
 		cout << "Guess a letter in the word ";
 		cout << output << " : ";
 		char guess;
+		// user's input guess
 		cin >> guess;
 
-		bool correct = false;
-		//bool duplicate = false;
-		for (int i = 0; i < word.length()-1; i++) {
+		bool correctGuess = false;
+		bool duplicate = false;
+		for (int i = 0; i < word.length(); i++) 
 			if (guess == word[i]) {
-				//if (output[i] == word[i]) {
-				  //cout << guess << " has already been guessed. \n" << endl;
-				  //duplicate = true;
-				  //break;
-				//} else {
+			   if (output[i] == word[i]) {
+			      cout << guess << " has already been guessed. \n" << endl;
+			      duplicate = true;
+			      break;
+			   } else {
 				output[i] = word[i];
 				numberGuessed++;
-				correct = true;
-				//}
+				correctGuess = true;
+				}
 			}
-			//if (duplicate)
-				//continue;
+			if (duplicate)
+			   continue;
 
-			if (correct == false) {
+			if (correctGuess == false) {
 				misses++;
 				cout << guess << " is not in the word! Try again. \n" << endl;;
 				}
 		}
-	}
+	
 	cout << "Correct! Word is " << word << "!" << endl; 
 	return misses;
 	
